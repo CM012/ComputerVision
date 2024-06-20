1da2efb8e2bb5592f9ca494e760ca13afacbcb0a
Charles_YAO <charles20041209@gmail.com>
2024-06-20 15:38:36 +0800

    6th_modify

--- Tutorial/Advanced_Projects/reco_car_plate_project/reco_car_plate.py
+++ Tutorial/Advanced_Projects/reco_car_plate_project/reco_car_plate.py
@@ -10,6 +10,19 @@ def cv_show(name, img):
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
+def sort_countours(cnts, method="left-to-right"):
+    reverse = False
+    i = 0
+
+    if method == "right-to-left" or method == "bottom-to-top":
+        reverse = True
+    
+    if method == "top-to-bottom" or method == "bottom-to-top":
+        i = 1
+    boundingBoxes = [cv2.boundingRect(c) for c in cnts]
+    (cnts, boundingBoxes) = zip(*sorted(zip(cnts, boundingBoxes),
+                                        key = lambda b: b[1][i], reverse=reverse))
+
 #Get one image
 img = cv2.imread(
     "/Users/charles/Documents/MyProjects/ComputerVision/Tutorial/Advanced_Projects/reco_car_plate_project/Materials/template.jpg")
@@ -28,3 +41,23 @@ ref_, refCnts, hierarchy = cv2.findContours(ref.copy(), cv2.RETR_EXTERNAL, cv2.C
 
 cv2.drawContours(img, refCnts, -1, (0,0,255), 3)
 cv_show('img', img)
+print(np.array(refCnts).shape)
+
+#To order the frames
+refCnts= myutils.sort_countours(refCnts, method="left-to-right")[0]
+digits={}
+
+# Every figure
+for (i, c) in enumerate(refCnts):
+    #resize the bounding box to the optimal size
+    (x, y, w, h) = cv2.boundingRect(c)
+    roi = ref[y:y + h, x:x + w]
+    roi = cv2.resize(roi, (57, 88))
+
+    #Every digit to each template
+    digits[i] = roi
+
+rectKernel = cv2.getStructuringElement(cv2.MORPH_RECT, (9, 3))
+sqlKernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5,5))
+
+image = cv2.imread(args["/Users/charles/Documents/MyProjects/ComputerVision/Tutorial/Advanced_Projects/reco_car_plate_project/Materials/credit_card.jpg"])
